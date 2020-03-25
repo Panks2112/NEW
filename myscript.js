bb2dac2aba3dfcbaec15db137a53ae4a2d01a9c5
Panks2112 <61877992+Panks2112@users.noreply.github.com>
2020-03-25 08:50:27 -0400

    [adding changes with new project]

---
+++ myscript.js
@@ -0,0 +1,12 @@
+ function mfunction()
+         {
+            var sel=document.getElementById('ss');
+            var p=sel.options[sel.selectedIndex];
+            var p1=parseInt(p.value);
+            console.log(p1);
+            // var x=document.getElementById("demo").value;
+            // var z=parseInt(x)*p1;
+            // console.log(z);
+            // //document.getElementById("s1").innerHTML="$" +z;
+         }
+        
